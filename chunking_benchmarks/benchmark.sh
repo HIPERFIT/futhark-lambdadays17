fe24b6380640f7450839be72cdf535e2280f6da6
Troels Henriksen <athas@sigkill.dk>
2016-12-14 15:25:50 +0100

    Bigger data sets for kmeans.

--- chunking_benchmarks/benchmark.sh
+++ chunking_benchmarks/benchmark.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if ! [ -f cluster-means.in ]; then
+    echo "Generating cluster_means.in (this may take a while)..."
+    futhark-dataset --i32-bounds 2000000:2000000 -g [5]int -g [10000000][3]f32 --i32-bounds 0:4 -g [10000000]int > cluster-means.in
+fi
+
 echo "Executing sequentially"
 echo "----------------------"
 futhark-bench sum-sobol-seq.fut
